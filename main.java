cf2b423bd074ca1a96b4c287a9394d4f5ed2db5f
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2024-01-05 13:38:25 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    
+    public static void main(String[] args) {
+       Pcmuhendisi pcmuh = new Pcmuhendisi(false);
+       pcmuh.adlisicil_sorgula();
+       pcmuh.mezuniyet_ort(3.10);
+       String[] array= {"Aselsan","Finansbank","Turksat" };
+       pcmuh.is_tecrubesi(array);
+  }
+}
