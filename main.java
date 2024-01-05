d84f47673d565837505a4f382f4aaf2aab1fb9e8
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2024-01-05 15:20:06 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,20 @@ public class main {
        Pcmuhendisi pcmuh = new Pcmuhendisi(false);
        pcmuh.adlisicil_sorgula();
        pcmuh.mezuniyet_ort(3.10);
+       System.out.println(pcmuh.mezuniyet_ort(3.10));
        String[] array= {"Aselsan","Finansbank","Turksat" };
        pcmuh.is_tecrubesi(array);
+
+       System.out.println("\n*********************\n");
+       
+       endustrimuh endustrimuh = new endustrimuh(true);
+       endustrimuh.adlisicil_sorgula();
+       endustrimuh.mezuniyet_ort(2.90);
+       String[] arrays={"HUAWEI", "Siemens"};
+       endustrimuh.is_tecrubesi(arrays);
+       String[] referans= {"Sumeyye Koyuncu"};
+       endustrimuh.referans_getir(referans);
+       
+        System.out.println(endustrimuh.mezuniyet_ort(2.90)); 
   }
 }
